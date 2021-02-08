d6b85dcf9199af610961953932c3b69709a19a0e
Tarsis de Lima <br.tmvdl@gmail.com>
2021-02-08 19:27:04 +0000

    Cordova Google Plus SDK plugin

--- app/init.sh
+++ app/init.sh
@@ -16,3 +16,7 @@ npm install --save @ionic/pwa-elements
 # Data Storage
 ionic cordova plugin add cordova-sqlite-storage
 npm install --save @ionic/storage
+
+# Cordova Google Plus SDK plugin
+ionic cordova plugin add cordova-plugin-googleplus --save --variable REVERSED_CLIENT_ID=YOUR_REVERSE_CLIENT_ID
+npm install --save @ionic-native/google-plus

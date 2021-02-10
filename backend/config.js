01ab3d939c206febb000d205d16143cffed0c039
Tarsis de Lima <br.tmvdl@gmail.com>
2021-02-10 23:15:03 +0000

    Adiconando Sequelize para conexão com o PostgreSQL

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  PORT: process.env.PORT || "3000",
+  database: [
+    process.env.DBNAME || "postgres",
+    process.env.DBUSER || "postgres",
+    process.env.DBPASS || "postgres",
+    {
+      host: process.env.DBHOST || "postgres",
+      port: process.env.DBPORT || "3000",
+      dialect: process.env.DBTYPE || "postgres",
+      logging: process.env.DBLOG || true ? console.info : () => {},
+    },
+  ],
 };

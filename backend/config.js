9ef6fda545f37d387dce17e7a68ab4e2b5cf9fc5
Tarsis de Lima <br.tmvdl@gmail.com>
2021-02-17 15:10:08 -0500

    Removing configuration for Sequelize

--- backend/config.js
+++ backend/config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  database: [
-    process.env.DBNAME || 'postgres',
-    process.env.DBUSER || 'postgres',
-    process.env.DBPASS || 'postgres',
-    {
-      host: process.env.DBHOST || 'postgres',
-      port: process.env.DBPORT || '3000',
-      dialect: process.env.DBTYPE || 'postgres'
-    }
-  ],
+  database: {
+    database: process.env.DBNAME || 'postgres',
+    username: process.env.DBUSER || 'postgres',
+    password: process.env.DBPASS || 'postgres',
+    dialect: process.env.DBTYPE || 'postgres',
+    hostname: process.env.DBHOST || 'postgres',
+    port: process.env.DBPORT || '3000'
+  },
   http: {
     port: process.env.PORT || '80'
   }
